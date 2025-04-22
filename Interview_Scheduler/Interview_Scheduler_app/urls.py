7007e764045065818b08a201667b41c8fc9e130d
unknown <aniketgoyal791@gmail.com>
2025-04-22 16:12:32 +0530

    Admin panel + forms for creating interviews

--- Interview_Scheduler/Interview_Scheduler_app/urls.py
+++ Interview_Scheduler/Interview_Scheduler_app/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index,name='index')
+    path('',views.dashboard,name='dashboard'),
+    path('applicants',views.applicants,name='applicants'),
+    path('add_applicant',views.add_applicant,name='add_applicant'),
+    path('slots',views.slots,name='slots'),
+    path('add_slot',views.add_slot,name='add_slot'),
+    path('assignments',views.assignments,name='assignments'),
+    path('add_assignment',views.add_assignment,name='add_assignment'),
 ]
--- Interview_Scheduler/Interview_Scheduler_app/views.py
+++ Interview_Scheduler/Interview_Scheduler_app/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from .models import Assignment,Applicant,InterviewSlot
 
 # Create your views here.
-def index(request):
-    return render(request,"index.html")
+def dashboard(request):
+
+    return render(request,"dashboard.html")
+
+def applicants(request):
+    applicants=Applicant.objects.all()
+    return render(request,'applicants.html',{"applicants":applicants})
+
+def slots(request):
+    interviews_slots=InterviewSlot.objects.all()
+    return render(request,'slots.html',{"interviews_slots":interviews_slots})
+
+def assignments(request):
+    assignments=Assignment.objects.select_related('interview_slot','applicant')
+    return render(request,'assignments.html',{"assignments":assignments})
+
+def add_applicant(request):
+    return render(request,'add_applicant.html')
+
+def add_slot(request):
+    return render(request,'add_slot.html')
+
+def add_assignment(request):
+    return render(request,'add_assignment.html')
