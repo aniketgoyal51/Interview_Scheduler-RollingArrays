8da308a05e288a68ab6321c662748985a562913f
aniketgoyal51 <141112246+aniketgoyal51@users.noreply.github.com>
2025-05-06 13:58:40 +0530

    Update settings.py

--- Interview_Scheduler/Interview_Scheduler/settings.py
+++ Interview_Scheduler/Interview_Scheduler/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["interview-scheduler-rollingarrays.onrender.com", 'localhost', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://interview-scheduler-rollingarrays.onrender.com',
+]
 
 # Application definition
 
